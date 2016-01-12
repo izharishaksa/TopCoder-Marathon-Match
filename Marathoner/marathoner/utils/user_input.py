41a8a1c13c0fd3a4a84d3a675f0ec35daa074078
Izhari Ishak Aksa <izharishaksa@gmail.com>
2016-01-12 21:17:12 +0700

    Remove redundant files.

--- Marathoner/marathoner/utils/user_input.py
+++
@@ -1,8 +0,0 @@
-from six.moves import input
-
-
-def get_input(msg, valid_values):
-    while True:
-        result = input(msg+' ').lower()
-        if result in valid_values:
-            return result
